373d1b71617e093e911814512b3b4b1a5f388265
GBS <gustavobragaia12@gmail.com>
2024-11-21 02:44:54 -0300

    path to upd ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,12 @@
 export function update( { request, response, database}){
-    return response.end("ok")
+    
+    const { id } = request.params
+    const { equipment, description } = request.body
+
+    database.update("tickets", id, {
+        equipment,
+        description,
+        updated_at: new Date(),
+    })
+    return response.end()
 }
--- src/utils/parseRoutePath.js
+++ src/utils/parseRoutePath.js
@@ -1,11 +1,11 @@
 export function parseRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g;
 
-    // Substitui parâmetros da rota por regex válido
+    // Substitui parâmetros da rota por regex com grupos nomeados válidos
     const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9-_]+)");
 
-    // Gera o regex final para a rota
-    const pathRegex = new RegExp(`^${params}(\\?(.*))?$`);
+    // Gera o regex final para a rota, incluindo query string opcional com grupo nomeado
+    const pathRegex = new RegExp(`^${params}(\\?(?<query>.*))?$`);
 
     return pathRegex;
 }
